9cc78e7dd3ee3cd0b07a9a033b8ea60c2ac4bf05
kiranaay <kirana.ndriyani@gmail.com>
2021-11-21 22:20:12 +0700

    perum login I

---
+++ application/views/view_latihan1.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    Halo.. yuk belajar web programming!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan degan metode penjumlah yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+    
+</body>
+</html>
